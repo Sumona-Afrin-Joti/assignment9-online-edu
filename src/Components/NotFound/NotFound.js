2220f7f55618df77e915742a08833e750e601789
Sumona-Afrin-Joti <sumonajoti.06@gmail.com>
2021-10-04 23:39:13 +0600

    fifth commit

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import img from '../../images/404.jpg';
-
+import Button from 'react-bootstrap/Button';
 const NotFound = () => {
     return (
         <div>
             <div className="w-50 mx-auto pt-5">
                 <img className="w-100" src={img} alt="" />
             </div>
+            <div className="text-center my-3">
+            <Link to="/home"> <Button className=" btn btn-primary form-label" type="submit">
+                Back to Home
+            </Button></Link>
+            </div>
+           
         </div>
 
     );
